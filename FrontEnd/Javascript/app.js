a6402b5b04ffc45bd2ae8708085da3fac3374253
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-08-20 16:01:58 +0300

    Added new Code

--- FrontEnd/Javascript/app.js
+++ FrontEnd/Javascript/app.js
@@ -1,2 +1,18 @@
-let addTwoNUmber = 2+2;
-console.log("isku darka xisaabtan waa : "+ addTwoNUmber);
+// let login = document.querySelector('login');
+// let singup = document.querySelector('singup');
+
+
+
+// function get(){
+//     if(login === "welcome"){
+//         alert("wad ku mahadsantahay")
+//     }else{
+//         alert("wanu ka xunahay ma hayno")
+//     }
+// }
+
+
+// expression
+let mustafe = 24;
+let b = mustafe * 4;
+console.log(b)
