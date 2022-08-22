3a0cb00337b706d160ab19b65edfa21631848151
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-08-22 18:15:39 +0300

    Added new Features and update more

--- FrontEnd/typescript/class/customer.ts
+++ FrontEnd/typescript/class/customer.ts
@@ -7,10 +7,10 @@ class customer{
     address:string;
     City:string;
     constructor( id:number,firstname:string,middleName:string,lastName:string,Phone:number,address:string,City:string){
-       this.id,
-        this.firstName = firstname,
-        this.middleName = this.middleName,
-        this.lastName = this.lastName,
+       this.id = id;
+        this.firstName = firstname;
+        this.middleName = middleName;
+        this.lastName = lastName,
         this.Phone  = Phone,
         this.address = address,
         this.City = City
@@ -19,7 +19,6 @@ class customer{
         return `id: ${this.id} firstname : ${this.firstName}  MiddleName: ${this.middleName} lastName: ${this.lastName} Phone: ${this.Phone} address: ${this.address} city: ${this.City}`;
     }
 }
-
 
 let customer1  = new customer(1,'osmaan','daahir','Ige',44333234,'Hawaadle','hargeisa');
 console.table(customer1);
