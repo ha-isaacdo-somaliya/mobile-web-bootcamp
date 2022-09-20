fdc4de3fcf87b8b939995c0e74bdb1420790361a
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-09-21 02:37:03 +0300

    wali shaqo ayaa ka socota waxaan ku daray qaybo dhawr ah sida links iyo mobileResponsive ...

--- FrontEnd/Tailwindcss/Challenges/ch4/tailwind.config.js
+++ FrontEnd/Tailwindcss/Challenges/ch4/tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
       'lg': '1024px',
       'xl': '1228px',
       '2xl': '1536px'
-    }
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      inter: ['Inter' ,'sans-serif']
+    },
   },
   plugins: [],
 }
