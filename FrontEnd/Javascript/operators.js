ca58c99b9cfaef602f9c73833103200e2377216e
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-09-22 15:15:24 +0300

    qayb ayaan ku daray

--- FrontEnd/Javascript/operators.js
+++ FrontEnd/Javascript/operators.js
@@ -26,3 +26,30 @@ let division = i / o;
 console.log('kala qaybinta labadan tiro waa : '+ division);
 
 
+// Comparison Operators;
+
+// Greather then 
+let age1 = 40;
+let age2 = 50;
+let result = age1 > age2;
+console.log('age1 wuu ka waynyahay age2 : '+ ' ' +result);
+
+
+// Lesthen 
+let age3 = 50;
+let age4 = 30;
+let result1 = age4 < age3;
+console.log('age4 wuu ka yaryahay age3 : '+ ' '+ result1);
+
+// greather then ore equal;
+let age5 = 90;
+let age6 = 20;
+let result2 = age5 >= age6;
+console.log('age5 wuu ka waynyahay age6 : '+ ' '+ result2);
+
+// Lessthen  or Equal
+let age7 = 80;
+let age8 = 60;
+let result3 = age8 <= age7;
+console.log('age7 wuu ka yaryahay age8 : '+ ' '+ result3);
+
